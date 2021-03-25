9ae78acaadafe6132b7bccb422bb7d7d8b2db001
Kelvin Andersen <kelvin.andersen5@gmail.com>
2021-03-26 01:32:29 +0700

    show only error message

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+grep -oP '(?<=ticky: ).*' syslog.log > text.txt
+grep -oP '(?<=ERROR ).*(?= \()' syslog.log | sort -u > error1.txt
 echo "Connection to DB failed,$(grep "ERROR Connection" syslog.log | wc -l)" >> error.csv
 echo "Permission denied while closing ticket,$(grep "ERROR Permission" syslog.log | wc -l)" >> error.csv
 echo "The ticket was modified while updating,$(grep "ERROR The" syslog.log | wc -l)" >> error.csv
@@ -8,3 +10,4 @@ echo "Timeout while retrieving information,$(grep "ERROR Timeout" syslog.log | w
 echo "Tried to add information to closed ticket,$(grep "ERROR Tried" syslog.log | wc -l)" >> error.csv
 echo "$(cat error.csv | sort -t, -k 2 -n -r)" > error_message.csv
 rm error.csv
+
