36aadde3a54e81cb7d60b823d7b30a7ef74e98f1
Kelvin Andersen <kelvin.andersen5@gmail.com>
2021-03-26 23:54:23 +0700

    soal 1 selesai

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 grep -oP '(?<=ticky: ).*' syslog.log > text.txt
+
 grep -oP '(?<=ERROR ).*(?= \()' syslog.log | sort -u > temp1.txt
 n=$(cat temp1.txt | wc -l)/2
 for ((i=0; i<n; i=i+1))
@@ -12,9 +13,24 @@ do
 	grep -v "$(grep -m1 "" temp2.txt)" temp2.txt > temp1.txt
 	> temp2.txt
 done
-echo "$(cat error.txt | sort -t, -k 2 -n -r)" > error_message.csv
+echo "Error,Count" > error_message.csv
+echo "$(cat error.txt | sort -t, -k 2 -n -r)" >> error_message.csv
 rm temp1.txt
 rm temp2.txt
 rm error.txt
 
+echo "Username,INFO,ERROR" > user_statistic.csv
+grep -oP '(\().*(?=\))' syslog.log | sort -u | cut -c 2- > temp1.txt
+n=$(cat temp1.txt | wc -l)/2
+for ((i=0; i<n; i=i+1))
+do
+	echo "$(grep -m1 "" temp1.txt),$(grep -w "INFO" syslog.log | grep -w "$(grep -m1 "" temp1.txt)" | wc -l),$(grep -w "ERROR" syslog.log | grep -w "$(grep -m1 "" temp1.txt)" | wc -l)" >> user_statistic.csv
+	grep -v -w "$(grep -m1 "" temp1.txt)" temp1.txt > temp2.txt
+	> temp1.txt
+	echo "$(grep -m1 "" temp2.txt),$(grep -w "INFO" syslog.log | grep -w "$(grep -m1 "" temp2.txt)" | wc -l),$(grep -w "ERROR" syslog.log | grep -w "$(grep -m1 "" temp2.txt)" | wc -l)" >> user_statistic.csv
+	grep -v -w "$(grep -m1 "" temp2.txt)" temp2.txt > temp1.txt
+	> temp2.txt
+done
+rm temp1.txt
+rm temp2.txt
 
