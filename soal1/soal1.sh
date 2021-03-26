35876722bab0b02a8cdf98b2723400848894dd74
Kelvin Andersen <kelvin.andersen5@gmail.com>
2021-03-26 18:56:37 +0700

    soal (d) selesai

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 grep -oP '(?<=ticky: ).*' syslog.log > text.txt
-grep -oP '(?<=ERROR ).*(?= \()' syslog.log | sort -u > error1.txt
-echo "Connection to DB failed,$(grep "ERROR Connection" syslog.log | wc -l)" >> error.csv
-echo "Permission denied while closing ticket,$(grep "ERROR Permission" syslog.log | wc -l)" >> error.csv
-echo "The ticket was modified while updating,$(grep "ERROR The" syslog.log | wc -l)" >> error.csv
-echo "Ticket doesn't exist,$(grep "ERROR Ticket" syslog.log | wc -l)" >> error.csv
-echo "Timeout while retrieving information,$(grep "ERROR Timeout" syslog.log | wc -l)" >> error.csv
-echo "Tried to add information to closed ticket,$(grep "ERROR Tried" syslog.log | wc -l)" >> error.csv
-echo "$(cat error.csv | sort -t, -k 2 -n -r)" > error_message.csv
-rm error.csv
+grep -oP '(?<=ERROR ).*(?= \()' syslog.log | sort -u > temp1.txt
+n=$(cat temp1.txt | wc -l)/2
+for ((i=0; i<n; i=i+1))
+do
+	echo "$(grep -m1 "" temp1.txt),$(grep "$(grep -m1 "" temp1.txt)" syslog.log | wc -l)" >> error.txt
+	grep -v "$(grep -m1 "" temp1.txt)" temp1.txt > temp2.txt
+	> temp1.txt
+	echo "$(grep -m1 "" temp2.txt),$(grep "$(grep -m1 "" temp2.txt)" syslog.log | wc -l)" >> error.txt
+	grep -v "$(grep -m1 "" temp2.txt)" temp2.txt > temp1.txt
+	> temp2.txt
+done
+echo "$(cat error.txt | sort -t, -k 2 -n -r)" > error_message.csv
+rm temp1.txt
+rm temp2.txt
+rm error.txt
+
 
