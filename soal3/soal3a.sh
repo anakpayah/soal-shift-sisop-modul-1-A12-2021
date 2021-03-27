20906922b3f1eeaafaedaee1ec4f285b6aefd0ab
Kelvin Andersen <kelvin.andersen5@gmail.com>
2021-03-27 21:33:54 +0700

    salah pas rename gatau kenapa

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -5,6 +5,19 @@ do
 	wget https://loremflickr.com/320/240/kitten -a foto.log
 done
 
-find -type f -exec md5sum '{}' ';' | sort | uniq --all-repeated=separate -w 15 | awk -F/ '{print $2}' dupes.txt
+echo "x" > dupes.txt
+while [ -s dupes.txt ]
+do
+	rm dupes.txt
+	find -type f -exec md5sum '{}' ';' | sort | uniq --all-repeated=separate -w 15 | awk -F/ '{print $2}' > dupes.txt
+	if [ -s dupes.txt ] 
+	then 
+		rm $(grep -m1 "" dupes.txt)
+	fi
+done
+rm dupes.txt
 
-
+for ((j=1; j<=$(ls -l | awk '{print $NF}' | grep "kitten" | wc -l); j=j+1))
+do
+	mv $(ls -l | awk '{print $NF}' | grep -m1 "kitten") Koleksi_$j
+done
