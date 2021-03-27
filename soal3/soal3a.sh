df3215f57f5bcd7fba63ae9c6bd7909e7063e924
Kelvin Andersen <kelvin.andersen5@gmail.com>
2021-03-27 20:02:08 +0700

    print duplicate file names

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+for ((i=0; i<23; i=i+1))
+do
+	wget https://loremflickr.com/320/240/kitten -a foto.log
+done
+
+find -type f -exec md5sum '{}' ';' | sort | uniq --all-repeated=separate -w 15 | awk -F/ '{print $2}' dupes.txt
+
 
